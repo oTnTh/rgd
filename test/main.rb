ef8d8ceb84b69c675a2f70077d7d335001795cc8
oCameLo <oCameLo@oTnTh>
2010-12-29 20:35:49 +0800

    change require order to fix warning

--- test/main.rb
+++ test/main.rb
@@ -4,8 +4,8 @@ FILE_PATH = File.expand_path(__FILE__)
 FILE_DIR = File.dirname(FILE_PATH)
 
 begin
-  require File.join(FILE_DIR, '../lib/rgd/version')
   require File.join(FILE_DIR, '../ext/rgd/rgd')
+  require File.join(FILE_DIR, '../lib/rgd/version')
 rescue Exception => e
   require 'rubygems'
   require 'rgd'
